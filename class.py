e368dabb0b933aee4572bb3e3dee210d94b06a31
MatthewTran23 <115042757+MatthewTran23@users.noreply.github.com>
2022-11-15 12:45:35 -0800

    brainstorm step by step

---
+++ class.py
@@ -0,0 +1,3 @@
+# libraries
+
+# game loop
---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+Penguin Slide final Project
+
+discription: a obsticle based platformer with the goal of making a user accesssable playing experience 
+
+required files: sprites import, main.py, class.py, settings.py
+
+libraries: pygame, more depends
+
+Construction Steps
+    -Add needed libraries in all files
+    -Make game loop in Main.py
+    -Make classes in class.py
+    -Add window and global variable settings in settings.py
+
+
+'''
+# libraries
+
+# game loop
