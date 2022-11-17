d5914c946b9dce686c0567411cab6c368aabdd61
MatthewTran23 <115042757+MatthewTran23@users.noreply.github.com>
2022-11-17 11:14:46 -0800

    update

--- setting.py
+++ setting.py
@@ -14,9 +14,9 @@ HEIGHT = 720
 FPS = 30
 mpos = (0,0)
 
-# define colors
+# define color
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+BLACK = (0, 0, 0) 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
