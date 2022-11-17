6cf5bdfb5106e285077e708b02bb40ed7519a2d2
MatthewTran23 <115042757+MatthewTran23@users.noreply.github.com>
2022-11-17 11:12:58 -0800

    update

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ required files: sprites import, main.py, class.py, settings.py
 
 libraries: pygame, more depends
 
-Sources: to be determined 
+Sources: 
+    -https://www.pygame.org/docs/
+    -
 
 Construction Steps
     -Add needed libraries in all files
@@ -18,6 +20,10 @@ Construction Steps
 
 '''
 # libraries
+import pygame# import libraries and modules
+import pygame as pg
+from pygame.sprite import Sprite
+from random import randint
 
 # game loop
 
--- setting.py
+++ setting.py
@@ -1,3 +1,22 @@
 # libraries
-
+import pygame# import libraries and modules
+import pygame as pg
+from pygame.sprite import Sprite
+from random import randint
 # settings
+
+# short hand for pygame vector for ease of use
+vec = pg.math.Vector2
+
+# game settings 
+WIDTH = 500
+HEIGHT = 720
+FPS = 30
+mpos = (0,0)
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
